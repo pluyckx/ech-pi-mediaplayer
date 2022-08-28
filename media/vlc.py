b2b0a544bda6af3b65ac7b0fc2df6cff97dcb8d0
Philip Luyckx <philip.luyckx@gmail.com>
2022-08-28 12:49:05 +0000

    Some more improvements and ability to loop
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- media/vlc.py
+++ media/vlc.py
@@ -1,4 +1,5 @@
 
+import os
 import subprocess
 from threading import RLock
 from typing import Optional
@@ -11,25 +12,67 @@ class Vlc(object):
 
     def is_running(self) -> bool:
         with self._lock:
-            return (self._process is not None) and (self._process.returncode is None)
+            running = (self._process is not None) and (self._process.returncode is None)
+
+            print(f"Process running: {running}")
+            print(f"process active: {self._process is not None}")
+            print(f"returncode: {'not running' if self._process is None else self._process.returncode}")
+            print("----")
+
+            return running
 
     def play(self, path: str) -> bool:
-        if self.is_running():
-            print("Not playing, vlc already running.")
-            return False
+        return self.__play(path, False)
 
+    def play_loop(self, path: str) -> bool:
+        return self.__play(path, True)
+
+    def __play(self, path: str, loop: bool = False) -> bool:
         with self._lock:
+            if self.is_running():
+                print("Not playing, vlc already running.")
+                return False
+
+            if not os.path.exists(path):
+                print(f"'{path}' does not exists!")
+                return False
+
+            if not os.path.isfile(path):
+                print(f"'{path}' is not a file!")
+                return False
+
+
             print(f"Play {path}")
-            self._process = subprocess.Popen([ "cvlc", "--fullscreen", "--no-osd", "--play-and-exit", path ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            cmd = [ "cvlc", "--fullscreen", "--no-osd", "--play-and-exit" ]
+
+            if loop:
+                cmd.append("--loop")
+
+            cmd.append(path)
+            self._process = subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            self._path = path
 
     def stop(self, path: str) -> bool:
         with self._lock:
+            print(f"{self.is_running()} '{self._path}' == '{path}'")
+
             if self.is_running() and (self._path == path):
                 print(f"Stopping {path}")
-                self._process.kill()
+                self._process.terminate()
+
+                try:
+                    self._process.wait(2.0)
+                except subprocess.TimeoutExpired:
+                    self._process.kill()
+
+                self._process = None
 
     def current_video(self) -> Optional[str]:
         if self.is_running():
             return self._path
         else:
             return None
+
+    def wait(self):
+        if self._process is not None:
+            self._process.wait()
